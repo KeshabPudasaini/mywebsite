bf11c5b8e92dfdd3cc71a566a891f37b3af7eb45
unknown <keshab.pudasaini3@gmail.com>
2025-04-01 13:28:59 +0545

    keshab

--- src/components/Bot/Bot.jsx
+++ src/components/Bot/Bot.jsx
@@ -3,12 +3,13 @@ import {Link} from 'react-scroll';
 
 function Bot() {
   return (
+    <div></div>
     
-    <div className=' fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-primary w-16 h-16 flex items-center justify-center rounded-full animate-bounce '>
-     <Link to="contact" spy ={true} activeClass="active" smooth={true} offset={-100}>
-     <ion-icon name="chatbubble-ellipses"></ion-icon>
-     </Link>
-    </div>
+    // <div className=' fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-primary w-16 h-16 flex items-center justify-center rounded-full animate-bounce '>
+    //  <Link to="contact" spy ={true} activeClass="active" smooth={true} offset={-100}>
+    //  <ion-icon name="chatbubble-ellipses"></ion-icon>
+    //  </Link>
+    // </div>
   )
 }
 
