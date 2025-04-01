35b261ee66a97da800d9e9358e1ce44a6da0bee3
unknown <keshab.pudasaini3@gmail.com>
2025-04-01 15:14:07 +0545

    update rosni

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,13 +6,13 @@ export default function About() {
   return (
     <div className="lg:mx-12 mx-4 my-20" id="about">
       <div className="flex flex-col sm:flex-row md:gap-24 items-center justify-between py-10">
-        <div className="sm:w-1/2">
+        <div className="sm:w-[40%]  ">
           <img src={photo0} alt="" className=" imgShadow2" />
         </div>
-        <div className=" sm:w-1/2">
+        <div className=" sm:w-[60%]">
           <p className="text-xl font-semibold mb-5">About</p>
           <h2 className="md:text-6xl text-4xl font-bold">About Me</h2>
-          <p className="mt-8 md:pr-8 mb-8 text-xl">
+          <p className="mt-8 md:pr-8  text-xl text-justify">
             Hi, I'm Mrs. Roshani Giri ,As a dedicated and detail-oriented Radiographer, 
             I am committed to providing high-quality imaging services to help diagnose and treat patients effectively. 
             With 7+ years of experience in performing a wide range of radiographic procedures, including X-rays,
